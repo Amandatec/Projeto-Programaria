32c9c8edcc67802adc0021e7a15b91349b107d4a
Amanda Oliveira <amanda.oliv@hotmail.com>
2020-11-17 01:16:01 +0000

    criação pasta javascript, IF, ELSE, &&, getElementById

---
+++ css/js/script.js
@@ -0,0 +1,7 @@
+if (document.getElemntById("nome") && o campo de email estiver preenchido && document.getElementById("email")estiver preenchido){
+  envie os dados
+}else{
+  peça para o usuário que preencha os campos nome e email
+}
+
+
