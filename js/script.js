dc1896a1bacdef031b5c266711874bb1c7a6be07
Amanda Oliveira <amanda.oliv@hotmail.com>
2020-11-17 01:26:12 +0000

    js first steps

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 if (document.getElemntById("nome") && o campo de email estiver preenchido && document.getElementById("email")estiver preenchido){
   envie os dados
 }else{
-  peça para o usuário que preencha os campos nome e email
+  peça para o usuario que preencha os campos nome e email
 }
-
-
