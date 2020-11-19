6af380000a35723ef7795afc21ba2da9c523a4f5
Amanda Oliveira <amanda.oliv@hotmail.com>
2020-11-19 11:05:14 +0000

    Inserção do operador relacional diferente !=

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-if (document.getElemntById("nome").value && o campo de email estiver preenchido && document.getElementById("email").value estiver preenchido){
+if (document.getElemntById("nome").value && o campo de email != "" && document.getElementById("email").value != ""){
   envie os dados
 }else{
   peça para o usuario que preencha os campos nome e email
