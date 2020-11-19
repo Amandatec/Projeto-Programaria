9ad0611529ac256759b58059420fe60108f0ba5f
Amanda Oliveira <amanda.oliv@hotmail.com>
2020-11-19 12:00:49 +0000

    inseri o comando function e <script> para linkar o html com js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-if (document.getElemntById("nome").value && o campo de email != "" && document.getElementById("email").value != ""){
-  alert("Prontinho! Você receberá as mensagens por email.")
-}else{
-  alert("Por favor, preencha os campos nome e email.")
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+  if (document.getElemntById("nome").value && o campo de email != "" && document.getElementById("email").value != ""){
+    alert("Prontinho! Você receberá as mensagens por email.")
+  }else{
+    alert("Por favor, preencha os campos nome e email.")
+  }
 }
+
+
+
